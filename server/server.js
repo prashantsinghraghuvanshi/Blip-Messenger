cbbbecb17abc2f0cc8bfe53f2892675398c1106a
AbhishekShaarmaa <abhisheksharma620703@gmail.com>
2024-04-12 21:52:06 +0530

    message routes and controller added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from"./routes/message.routes.js";
 import connectToMongoDb from "./db/connectToMongoDb.js";
 
 const app   =express();
@@ -11,6 +12,7 @@ dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with json payloads from req.body 
 app.use("/api/auth" , authRoutes)
+app.use("/api/message" , messageRoutes)
 // app.get("/" , (req , res)=>{
 //     res.send("hello world//////")
 // })
