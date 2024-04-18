92adebc4121c86c0b8536d50de8d8d7e20a4d80e
Prashant Singh <singhprashant0314@gmail.com>
2024-04-18 09:13:00 +0530

    Bug fix #2 : Toast Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Toaster from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
