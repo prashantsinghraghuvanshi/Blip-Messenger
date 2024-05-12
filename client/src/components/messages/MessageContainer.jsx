dbe6e31f86e72201192fc86b22c4df2b5c73e3b7
Prashant Singh <singhprashant0314@gmail.com>
2024-05-12 21:21:25 +0530

    weekly update

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,9 +1,10 @@
 import MessageInput from "./MessageInput";
 import useConversation from "../../zustand/useConversation";
 import Messages from "./Messages";
+import { capitalize } from "lodash";
 import { useEffect } from "react";
 import { useAuthContext } from "../../context/AuthContext";
-// import { TiMessages } from "react-icons/ti";
+import { TbHexagonLetterB } from "react-icons/tb";
 
 export default function MessageContainer() {
   const { selectedConversation, setSelectedConversation } = useConversation();
@@ -41,13 +42,9 @@ const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-xl md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome {authUser.userName}!</p>
+        <p>Hello {capitalize(authUser.userName)}!</p>
         <p>Select a chat to start messaging 🫡</p>
-        <img
-          className="object-contain mt-4 h-60 w-100 items-center"
-          src="../../dist/assets/final-logo.svg"
-          alt="Blip Logo"
-        />
+        <TbHexagonLetterB size={150} />
       </div>
     </div>
   );
