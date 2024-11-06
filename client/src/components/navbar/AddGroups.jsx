53df6566ba7123c9301d7d32bdc0807904a26af2
AbhishekShaarmaa <abhisheksharma620703@gmail.com>
2024-11-06 13:35:56 +0530

    get friends bug fixed

---
+++ client/src/components/navbar/AddGroups.jsx
@@ -0,0 +1,23 @@
+import { LiaUserFriendsSolid } from "react-icons/lia";
+import useConversation from "../../zustand/useConversation";
+
+export default function FriendsButton() {
+  const { selectFriends, setSelectFriends, setSelectedConversation } =
+    useConversation();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!selectFriends) return;
+    setSelectedConversation(null);
+    setSelectFriends();
+  };
+
+  return (
+    <div className="my-auto">
+      <LiaUserFriendsSolid
+        className="w-6 h-6 text-white cursor-pointer"
+        onClick={handleSubmit}
+      />
+    </div>
+  );
+}
--- server/controller/getUser.controller.js
+++ server/controller/getUser.controller.js
@@ -15,53 +15,45 @@ export const getGlobalUsers = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
 export const getUsers = async (req, res) => {
- 
-    try {
-      const loggedInUserId = req.user._id.toString();
-  
-      // Find conversations where the logged-in user participates
-      const conversations = await Conversation.find({
-        participants: { $all: [loggedInUserId] },
-      }).sort({ updatedAt: -1 });
-      console.log("conversation" , conversations)
-  
-      const uniqueIdsSet = new Set();
-  
-      conversations.forEach(conversation => {
-        conversation.participants.forEach(id => {
-          // Convert ObjectId to string representation before adding to Set
-          uniqueIdsSet.add(id.toString());
-        });
+  try {
+    const loggedInUserId = req.user._id.toString();
+
+    // Find conversations where the logged-in user participates
+    const conversations = await Conversation.find({
+      participants: { $all: [loggedInUserId] },
+    }).sort({ updatedAt: -1 });
+    console.log("conversation", conversations);
+
+    const uniqueIdsSet = new Set();
+    conversations.forEach((conversation) => {
+      conversation.participants.forEach((id) => {
+        // Convert ObjectId to string representation before adding to Set
+        uniqueIdsSet.add(id.toString());
       });
-  
-      // Convert Set back to Array
-      let uniqueIdsArray = Array.from(uniqueIdsSet);
-  
-      // Remove loggedInUserId from the array
-      uniqueIdsArray = uniqueIdsArray.filter(id => id !== loggedInUserId);
-  
-      console.log(uniqueIdsArray);
-  
-      // Fetch users corresponding to unique participant IDs
-      const usersMap = {};
-      const users = await User.find({ _id: { $in: uniqueIdsArray } });
-      users.forEach(user => usersMap[user._id.toString()] = user);
-  
-      // Arrange users in the same order as uniqueIdsArray
-      const orderedUsers = uniqueIdsArray.map(id => usersMap[id]);
-  
-      orderedUsers.forEach(user => console.log("users", user._id));
-  
-      res.status(200).json(orderedUsers);
-    } catch (error) {
-      console.error("Error in getUser controller: ", error.message);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  };
-  
+    });
 
-  
+    // Convert Set back to Array
+    let uniqueIdsArray = Array.from(uniqueIdsSet);
 
-  
+    // Remove loggedInUserId from the array
+    uniqueIdsArray = uniqueIdsArray.filter((id) => id !== loggedInUserId);
+    console.log(uniqueIdsArray);
+
+    // Fetch users corresponding to unique participant IDs
+    const usersMap = {};
+    const users = await User.find({ _id: { $in: uniqueIdsArray } });
+    users.forEach((user) => (usersMap[user._id.toString()] = user));
+
+    // Arrange users in the same order as uniqueIdsArray and filter undefined values
+    const orderedUsers = uniqueIdsArray
+      .map((id) => usersMap[id])
+      .filter((user) => user);
+
+    orderedUsers.forEach((user) => console.log("users", user._id));
+    res.status(200).json(orderedUsers);
+  } catch (error) {
+    console.error("Error in getUser controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
